835c3965290a68bd173d7b9c35d271ae9c2312c3
kathir.v <kathir.v@zohocorp.com>
2019-04-12 17:37:16 +0530

    Composing Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,7 +14,7 @@ export default class Counter extends Component {
         return (
             <div>
                 <span className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
