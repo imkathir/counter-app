717a3a1bf806e5b960463874665dae9b65bb6181
kathir.v <kathir.v@zohocorp.com>
2019-04-12 18:49:04 +0530

    handle the state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,25 @@ import Counter from './counter'
 
 class Counters extends Component {
     state = {
+        totalCounters: 4,
         counters: [
             { id: 1, value: 4 },
             { id: 2, value: 0 },
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
+    };
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters, totalCounters: counters.length });
     }
     render() { 
         return ( 
             <div>
+                TotalCounter =<button className="btn btn-secondary btn-sm">{this.state.totalCounters}</button>
+
                 { this.state.counters.map(counter => 
-                    <Counter key={counter.id} value={counter.value}>
-                        <h1>Counter {counter.id} and Value is {counter.value}</h1>
+                    <Counter key={counter.id} value={counter.value} onDelete={this.handleDelete} counter={counter}>
                     </Counter>
                     ) }
             </div> 
